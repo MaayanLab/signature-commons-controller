d4488392fcbf698808e51536a735c048753ccf5d
Daniel J. B. Clarke <u8sand@gmail.com>
2020-01-07 15:34:40 -0500

    Enable s3 direct extraction of data files

---
+++ controller/extract/s3/__init__.py
@@ -0,0 +1,2 @@
+from controller.util.importdir import importdir
+importdir(__file__, __package__, globals())
---
+++ controller/extract/s3/s3_data_gmt_so.py
@@ -0,0 +1,33 @@
+import os
+import minio
+from controller.util.first import first
+
+outputs = (
+  '*.data.gmt.so',
+)
+
+def requirements(uri=[], **kwargs):
+  return 's3' in set([s for u in uri for s in u.scheme.split('+')])
+
+def extract(path=None, uri=[], **kwargs):
+  # Get s3 uri
+  s3_uri = first(u for u in uri if 's3' in u.scheme.split('+'))
+  s3_bucket = s3_uri.path[1:]
+  s3_access_key = s3_uri.username
+  s3_secret_key = s3_uri.password
+  del s3_uri.username
+  # Establish s3 connection
+  s3_client = minio.Minio(
+    s3_uri.netloc,
+    access_key=s3_access_key,
+    secret_key=s3_secret_key,
+    secure='https' in s3_uri.scheme,
+  )
+  # Find objects
+  for obj in s3_client.list_objects(s3_bucket):
+    if obj.object_name.endswith('.data.gmt.so'):
+      s3_client.fget_object(
+        s3_bucket,
+        obj.object_name,
+        os.path.join(path, obj.object_name)
+      )
