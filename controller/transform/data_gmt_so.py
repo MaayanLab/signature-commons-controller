18a3ac4aec6fa387e80b71be10286f1297eee9cf
Daniel J. B. Clarke <u8sand@gmail.com>
2020-01-07 15:26:48 -0500

    Add deserialization transformations

---
+++ controller/transform/data_gmt_so.py
@@ -0,0 +1,24 @@
+import os
+from subprocess import check_output, check_call
+
+inputs = (
+  '*.data.gmt.so',
+)
+outputs = (
+  '*.data.uuid.gmt',
+)
+
+def transform(input_files, output_files, **kwargs):
+  input_file, = input_files
+  output_file, = output_files
+  statinfo =  os.stat(input_file)
+  statinfo.st_size
+  # estimate required ram ~ 75% more than base file size
+  required_ram = int(statinfo.st_size * 1.75)
+  check_call([
+    'java', '-Xmx%d' % (required_ram),
+    '-jar', os.path.join(os.path.dirname(__file__), '..', 'SignatureCommonsDataIngestion.jar'),
+    '-d',
+    '-i', input_file,
+    '-o', output_file,
+  ])
