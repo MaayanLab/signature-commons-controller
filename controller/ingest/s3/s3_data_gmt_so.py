65368cb96729a5d418c4d1da8eb3c92d8c9f8980
Daniel J. B. Clarke <u8sand@gmail.com>
2020-01-07 15:34:57 -0500

    Naming consistency for s3 ingest

--- controller/ingest/s3/s3_data_gmt_so.py
+++ controller/ingest/s3/s3_data_gmt_so.py
@@ -18,14 +18,14 @@ def ingest(input_files, uri=[], **kwargs):
   s3_secret_key = s3_uri.password
   del s3_uri.username
   # Establish s3 connection
-  client = minio.Minio(
+  s3_client = minio.Minio(
     s3_uri.netloc,
     access_key=s3_access_key,
     secret_key=s3_secret_key,
     secure='https' in s3_uri.scheme,
   )
   # Upload object
-  client.fput_object(
+  s3_client.fput_object(
     s3_bucket,
     os.path.basename(so),
     so,
