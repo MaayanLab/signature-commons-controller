b488315dcfa53ada515b6cd68672981e0731675a
Daniel J. B. Clarke <u8sand@gmail.com>
2020-01-08 14:13:45 -0500

    Ensure minimum ram of 1G for data ingestion

--- controller/transform/data_uuid_T_tsv.py
+++ controller/transform/data_uuid_T_tsv.py
@@ -14,7 +14,7 @@ def transform(input_files, output_files, **kwargs):
   statinfo =  os.stat(input_file)
   statinfo.st_size
   # estimate required ram ~ 75% more than base file size
-  required_ram = int(statinfo.st_size * 1.75)
+  required_ram = int(max(1e9, statinfo.st_size * 1.75))
   check_call([
     'java', '-Xmx%d' % (required_ram),
     '-jar', os.path.join(os.path.dirname(__file__), '..', 'SignatureCommonsDataIngestion.jar'),
