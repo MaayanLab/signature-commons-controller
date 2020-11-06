2e01a77557159e1192f4a62146935bb79edbb990
Daniel Clarke <u8sand@gmail.com>
2020-11-06 12:34:45 -0500

    Update major version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='signature-commons-controller',
-  version='0.1.1',
+  version='0.2.0',
   url='https://github.com/MaayanLab/signature-commons-controller',
   author='Daniel J. B. Clarke',
   author_email='u8sand@gmail.com',
