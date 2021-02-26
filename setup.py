b69c4063235d927da27891e8a30d2822c6768a66
Daniel Clarke <u8sand@gmail.com>
2021-02-26 13:27:44 -0500

    Fix extract for s3 subpaths

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='signature-commons-controller',
-  version='0.2.1',
+  version='0.2.2',
   url='https://github.com/MaayanLab/signature-commons-controller',
   author='Daniel J. B. Clarke',
   author_email='u8sand@gmail.com',
