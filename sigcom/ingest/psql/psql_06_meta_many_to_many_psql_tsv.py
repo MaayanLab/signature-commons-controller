06db1b526a76c8a280bd570be2ea235546ef6498
erol <je.evangelista04@gmail.com>
2022-02-10 19:54:10 -0500

    do update

--- sigcom/ingest/psql/psql_06_meta_many_to_many_psql_tsv.py
+++ sigcom/ingest/psql/psql_06_meta_many_to_many_psql_tsv.py
@@ -38,7 +38,12 @@ def ingest(input_files, uri=[], limit=1000, **kwargs):
     insert into signatures_entities (signature, entity, direction, score, top_signatures, top_entities)
       select signature, entity, direction, score, top_signatures, top_entities
       from signatures_entities_tmp
-      on conflict DO UPDATE
+      on conflict (signature, entity, direction)
+        do update
+        set 
+          score = excluded.score,
+          top_signatures = excluded.top_signatures,
+          top_entities = excluded.top_entities,
     ;
   ''')
   cur.execute('drop table signatures_entities_tmp;')
